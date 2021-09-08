75ae6543fae4c987811e96ece7c8ab20dedb0221
gwangseon <cgs308@hotmail.com>
2021-09-08 09:57:52 +0900

    [info] 구성 설정 변경 및 메일 확인

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -45,7 +45,7 @@ $(function () {
                     //clear all fields
                     $("#contactForm").trigger("reset");
                 },
-                error: function (e) {
+                error: function () {
                     // Fail message
                     $("#success").html("<div class='alert alert-danger'>");
                     $("#success > .alert-danger")
@@ -57,7 +57,7 @@ $(function () {
                         $("<strong>").text(
                             "Sorry " +
                                 firstName +
-                                ", it seems that my mail server is not responding. Please try again later!" + e.message
+                                ", it seems that my mail server is not responding. Please try again later!"
                         )
                     );
                     $("#success > .alert-danger").append("</div>");
