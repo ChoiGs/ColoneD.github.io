835f790f4b6aaefd1ebb915f546ab3f02f610226
gwangseon <cgs308@hotmail.com>
2021-09-08 09:27:46 +0900

    [info] 구성 설정 변경 및 메일 확인

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -13,10 +13,10 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = "cgs308@hotmail.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: cgs308@naver.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$header .= "Reply-To: $email";	
+$contents = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: cgs308@naver.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email";	
 
-if(!mail($to, $subject, $body, $header))
+if(!mail($to, $subject, $contents, $header))
   http_response_code(500);
 ?>
